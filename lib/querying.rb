c15d03f6908ed98b6b1b67cd4b9b0dc3a2f321bf
Achsah Abraham <achsahmp@gmail.com>
2018-08-20 23:56:33 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,7 +15,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species
+  "SELECT species
   COUNT characters.species
   FROM characters
   GROUP BY characters.species
