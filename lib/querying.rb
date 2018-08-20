c79c68b68cea6389ea72dd9e1c538e29a94322ee
Achsah Abraham <achsahmp@gmail.com>
2018-08-20 23:58:13 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ end
 
 def select_value_and_count_of_most_prolific_species
   "SELECT characters.species
-  COUNT characters.species
+  COUNT characters.species AS num_species
   FROM characters
   GROUP BY characters.species
   ORDER BY characters.species DESC LIMIT 1;"
